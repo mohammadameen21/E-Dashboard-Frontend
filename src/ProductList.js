a684f34b422f9712e8db3059148919b5c99a5728
Ameen <md.ameen1080@gmail.com>
2022-06-16 11:56:24 +0530

    Now getting dynamic data from database

--- src/ProductList.js
+++ src/ProductList.js
@@ -28,13 +28,13 @@ const ProductList = () => {
             </ul>
 
             {
-                products.map(()=>
+                products.map((item,index)=>
                 
-                <ul>
-                    <li>S.No.</li>
-                    <li>Name</li>
-                    <li>Price</li>
-                    <li>Category</li>
+                <ul key={item}>
+                    <li>{index+1}</li>
+                    <li>{item.name}</li>
+                    <li>{item.price}</li>
+                    <li>{item.category}</li>
                 </ul>
                 
                 )

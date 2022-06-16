e6abf843b3502dff1265b815657dc246cfd517cd
Ameen <md.ameen1080@gmail.com>
2022-06-16 12:35:28 +0530

    Delete button deleting items in ui and db as well

--- src/ProductList.js
+++ src/ProductList.js
@@ -14,7 +14,16 @@ const ProductList = () => {
     }
 
 
-    console.log(products);
+    const deleteProduct= async (id)=>{
+        console.log(id)
+        let result = await fetch(`http://localhost:9800/product/${id}`,{
+            method: "Delete"
+        });
+        result = await result.json();
+        if(result){
+           getProducts();
+        }
+    }
 
 
     return (
@@ -25,16 +34,19 @@ const ProductList = () => {
                 <li>Name</li>
                 <li>Price</li>
                 <li>Category</li>
+                <li>Operation</li>
             </ul>
 
             {
                 products.map((item,index)=>
                 
-                <ul key={item}>
+                <ul key={item._id}>
                     <li>{index+1}</li>
                     <li>{item.name}</li>
                     <li>{item.price}</li>
                     <li>{item.category}</li>
+                    <li><button onClick={()=>deleteProduct(item._id)}>Delete</button></li>
+
                 </ul>
                 
                 )
